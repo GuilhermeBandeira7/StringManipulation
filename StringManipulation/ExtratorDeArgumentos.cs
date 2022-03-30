0c4b9b1f50c9d9851c3b8dde5c3ce1e1bf700fbe
Guilherme Bandeira <guilhermeprestes1@hotmail.com>
2022-03-30 15:18:45 -0300

    Project created to practice string manipulation

---
+++ StringManipulation/ExtratorDeArgumentos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    /// <summary>
+    /// Class that returns of arguments of a URL in string format
+    /// </summary>
+    public class ExtratorDeArgumentos
+    {
+        private readonly string _argumentos;
+        public string URL { get; }
+        /// <summary>
+        /// Represents a instance of ExtratorDeArgumentos
+        /// </summary>
+        /// <param name="url"> Represents the value of <see cref="URL"/></param>
+        /// <exception cref="ArgumentException"> Exception thrown when the <see cref="URL"/> is empty </exception>
+        
+        public ExtratorDeArgumentos(string url)
+        {
+
+            if (String.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("The URL can't be null or empty.", nameof(url));
+            }
+
+            URL = url;
+
+            int indiceInterrogacao = url.IndexOf('?'); // after the ? we have the arguments that we want to get the values 
+            _argumentos = url.Substring(indiceInterrogacao + 1); // _argumentos receive only the arguments of the url(string after ? sign)
+
+        }
+        /// <summary>
+        /// Method that returns the values of the parameters
+        /// </summary>
+        /// <param name="nomeParametro"> Represents the string parameter </param>
+        /// <exception cref="IndexOutOfRangeException"></exception>  
+        /// <returns></returns>
+        public string GetValor(string nomeParametro) // method used to trim a string returning the value of the parameter passed as argument
+        {
+            nomeParametro = nomeParametro.ToUpper(); // VALOR
+            string argumentoEmCaixaAlta = _argumentos.ToUpper();
+
+            string termo = nomeParametro + "="; // moedaDestino= "=" bacause in the url parameters the values comes after the = sign. And we are trying to get the value index
+            int indiceTermo = argumentoEmCaixaAlta.IndexOf(termo); // index of the 'm' the first character of moedaDestino
+
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length); // returns dolar
+            int indiceEComercial = resultado.IndexOf('&');
+
+            if (indiceEComercial == -1) // -1 used because when the indexof method doesn't find what we are looking for the output is -1. 
+            {
+                return resultado;
+            }
+
+            return resultado.Remove(indiceEComercial);  
+
+            // Temos o texto "moedaOrigem=real&moedaDestino=XXXX"
+            //Buscamos o índice do valor do parâmetro "moedaDestino"
+            //Vamos então usar IndexOf("moedaDestino") e, neste índice, somar o tamanho do texto "moedaDestino"
+
+        }
+    }
+}
---
+++ StringManipulation/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //// TESTANDO OS MÉTODOS substring E indexof
+            //string url = "pagina?argumentos";
+            //int indiceInterrogacao = url.IndexOf('?'); // the output is 6 witch is =  ?argumentos
+
+            //string argumentos = url.Substring(indiceInterrogacao + 1); // plus 1 the indexOf indiceInterrogacao is 7 witch results in = argumentos
+
+            //Console.WriteLine(argumentos); // argumentos output
+
+            //Console.WriteLine("==============");
+
+            //string palavra = "moedaOrigem=real&moedaDestino=dolar"; // full string
+            //string nomeArgumento = "moedaDestino";
+
+            //int indice = palavra.IndexOf(nomeArgumento); // returns the index of "moedaDestino" which is 17.
+
+            //int indiceValor = indice + nomeArgumento.Length;           
+
+            //string valorArgumento = palavra.Substring(indiceValor + 1); // which returns "dolar"
+
+            //Console.WriteLine(valorArgumento);
+
+            //Console.WriteLine("==============");
+
+            //// TESTANDO UMA URL E USANDO ExtratorDeArgumentos E O MÉTODO GetValor PARA OBTER VALORES DOS PARÂMETROS 
+
+            //string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar"; //URL 
+            //ExtratorDeArgumentos extratorDeValores = new ExtratorDeArgumentos(urlParametros); // instance of ExtratorDeArgumentos class to manipulate  strings
+            //string valor = extratorDeValores.GetValor("moedaOrigem"); // variable valor stores the output of GetValor method that retuns "Real"
+            //Console.WriteLine("Valor de moedaOrigem: " + valor);
+
+            //string valor2 = extratorDeValores.GetValor("moedaDestino");
+            //Console.WriteLine("Valor de moedaDestino: " + valor2); //variable valor2 stores the output of GetValor method that retuns "Dolar"
+
+            //string testeRemocao = "primeiraParte&parteParaRemover";           
+            //int indiceComercial = testeRemocao.IndexOf('&');
+            //Console.WriteLine(testeRemocao.Remove(indiceComercial)); // using remove method to cut the string e return only "primeiraParte"           
+
+            //Console.WriteLine("==============");
+
+            //// TESTANDO REPLACE
+
+            //string mensagemOrigem = "PALAVRA";
+            //string termoBusca = "ra";
+            //termoBusca = termoBusca.Replace('r' , 'R'); // replace needs an old char and a new char as argument. 
+            //Console.WriteLine(termoBusca);
+            //termoBusca = termoBusca.Replace('a', 'A');
+            //Console.WriteLine(termoBusca);
+
+            ////TESTANDO TO UPPER
+
+            //Console.WriteLine(mensagemOrigem.ToUpper());
+
+            //int resultado = mensagemOrigem.IndexOf(termoBusca);
+            //Console.WriteLine(resultado);
+            //Console.ReadLine();
+
+            URLTestes();
+            Console.ReadLine();
+        }
+
+        private static void URLTestes()
+        {
+            string urlTeste = "https://www.bytebank.com/cambio";
+            int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
+
+            Console.WriteLine(urlTeste.StartsWith("https://www.bytebank.com"));
+            Console.WriteLine(urlTeste.StartsWith("cambio"));
+            Console.WriteLine(urlTeste.Contains("bytebank"));
+
+            //Console.WriteLine(indiceByteBank == 0);  //when indexof does not find an index it returns -1. This logic is applied to see if indexof found our index                      
+        }
+    }
+}
